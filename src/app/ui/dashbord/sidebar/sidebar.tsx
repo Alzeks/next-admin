8298347e57ec9c816c5e518e825436f52ef17200
ALZEX <solo-nick@i.ua>
2024-08-26 14:14:06 +0300

    add src

--- src/app/ui/dashbord/sidebar/sidebar.tsx
+++ src/app/ui/dashbord/sidebar/sidebar.tsx
@@ -72,21 +72,20 @@ const menuItems = [
 ];
 
 const Sidebar = async () => {
-  const { user } = await auth();
-console.log('sidebar',user);
-
+  const session  = await auth();
+  
   return (
     <div className={styles.container}>
       <div className={styles.user}>
         <Image
           className={styles.userImage}
-          src={user.image || "/noavatar.png"}
+          src={session?.user.image || "/noavatar.png"}
           alt=""
           width="50"
           height="50"
         />
         <div className={styles.userDetail}>
-          <span className={styles.username}>{user.username}</span>
+          <span className={styles.username}>{session?.user.username}</span>
           <span className={styles.userTitle}>Administrator</span>
         </div>
       </div>
